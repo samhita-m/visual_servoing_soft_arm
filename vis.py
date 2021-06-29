ed92638efff8fc12ec35c549a9506ce9015199cf
samhita-m <samhitamarri@gmail.com>
2021-06-29 18:42:25 -0500

    4. Metric Plot Done

--- vis.py
+++ vis.py
@@ -7,32 +7,23 @@ Created on Tue Jun 29 16:17:31 2021
 """
 
 import os
-
 import numpy as np
-
 import matplotlib.pyplot as plt
-
 from PIL import Image
+from skimage import img_as_float
 
-
-
-
+from skimage.measure import compare_mse
 
 
 def plot_actuation(iterations, actuation_data, path):
     
     fig, ax = plt.subplots(1, 1, figsize=(10, 5)) 
-    
     x = iterations
-    
     legends = ['P_B', 'P_R', 'P_theta', 'x', 'y']
     
-
     for i in range(actuation_data.shape[0]):
         y = actuation_data[i]
         plt.plot(x, y) 
-        
-      
     
     ax.grid()
     ax.set_xlabel('Iteration')
@@ -45,8 +36,6 @@ def plot_actuation(iterations, actuation_data, path):
         os.makedirs(os.path.join('images/', path))
     plt.savefig(os.path.join('images/', path, 'actuation_curve.png'))
     
-    
-    
 
 def plot_superimposition(img1_path, img2_path, path):
 
@@ -58,7 +47,39 @@ def plot_superimposition(img1_path, img2_path, path):
     
     new_img = Image.blend(background, overlay, 0.5)
     new_img.save(os.path.join('images/', path, 'superimposition.png'),"PNG")
+    
+    # probably plot current image, final image and superimposition in subplots
+    
 
+def plot_mse_error(current_images_path, final_image_path, path):
+    
+    iterations = [x for x in range(len(current_images_path))]
+
+    mse_list = []
+    img_final = Image.open(os.path.join('images/', path, final_image_path))
+    for x in current_images_path:
+        
+        img1 = Image.open(os.path.join('images/', path, x))
+        
+        mse = compare_mse(img_as_float(img1), img_as_float(img_final))
+        
+        mse_list.append(mse)
+        
+    
+    fig, ax = plt.subplots(1, 1, figsize=(10, 5)) 
+    
+    plt.plot(iterations, mse_list)
+    
+    ax.grid()
+    ax.set_xlabel('Iteration')
+    ax.set_ylabel('MSE')
+    ax.set_title('MSE vs Iteration')  
+    plt.tight_layout()
+    
+    plt.savefig(os.path.join('images/', path, 'msecurve.png'))
+        
+    
+    
 
 num_iterations = 10
 
